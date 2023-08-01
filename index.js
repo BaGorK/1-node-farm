448e856695842282512a300457ff4c07dbadc84b
BaGorK <edmealemkassahun@gmail.com>
2023-07-31 21:25:25 -0700

    Building a very simple api

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 
-// http.createServer(function (req, res) {
-//   return res.end('Hello from the server!!!');
-// });
-const server = http.createServer((req, res) => {
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8'); // excuted only once
+const dataObj = JSON.parse(data); // This converts the json file to a javascript object
+
+const server = http.createServer((req, res) => { // excuted per request
   const pathName = req.url;
+  
   if (pathName === '/' || pathName === '/overview') {
     res.end('This is the overview');
   } else if (pathName === '/product') {
     res.end('This the Product');
+  } else if (pathName === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
